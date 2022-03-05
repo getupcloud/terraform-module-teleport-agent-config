5fc28091e2a840a12d90b2cba19c1cfb38b12bf7
Mateus Caruccio <mateus.caruccio@getupcloud.com>
2022-03-05 14:27:52 -0300

    missing teleport_auth_token

--- output.tf
+++ output.tf
@@ -1,5 +1,6 @@
 output "teleport_agent_config" {
   value = {
+    teleport_auth_token : var.auth_token
     teleport_kube_cluster_name : "${var.customer_name}-${var.cluster_name}"
     teleport_proxy_addr : var.proxy_addr
     teleport_labels : merge({
